d3ec18b4212f46cb73ddd0a2ae76abf2b8b5ce78
430009048 <430009048@H41780LAPLGP>
2024-03-02 13:02:11 -0600

    Agregar archivos de proyecto.

---
+++ Controllers/HelloWorldController.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Text.Encodings.Web;
+
+
+namespace MVCApp.Controllers
+{
+	public class HelloWorldController : Controller
+	{
+		public IActionResult index()
+		{
+			return View();
+		}
+
+		public IActionResult Welcome(string name,int numTimes=1)
+		{
+			ViewData["Message"] = "Hello " + name;
+			ViewData["NumTimes"] = numTimes;
+			return View();
+		}
+
+		//public string index()
+		//{
+		//    return "this is my default action...";
+
+
+		//}
+
+		//Get: /HelloWorld/Welcome/
+		//Requieres using System.Text.Encodings.Web;
+		//Get: /HelloWorld/Welcome?name=analizeth&numTimes=5/
+
+		//public string Welcome(string name, int numTimes=1)
+		//{
+		//    //return "This is the welcome action method...";
+		//    return HtmlEncoder.Default.Encode($"Hello {name}, numTimes is:{numTimes}");
+		//}
+	}
+
+}
---
+++ Models/MovieGenreViewModel.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using System.Collections.Generic;
+using System.Security.Permissions;
+
+namespace MVCApp.Models
+{
+	public class MovieGenreViewModel
+	{
+		//signo de ? puede o no que tenga datos nuestra lista
+		public List<Movie>? Movies { get; set; }
+		public SelectList? Genres { get; set; }
+		public string? MovieGenre { get; set; }
+		public string? SearchString { get; set; }
+
+
+	}
+}
